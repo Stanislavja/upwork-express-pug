8fbce80f3008172cd0ec48ac023dc23aa3c38833
Stanislav <stanislavja@yandex.ru>
2021-08-17 01:33:21 +0400

    2st task

--- src/index.js
+++ src/index.js
@@ -48,4 +48,32 @@ app.get("/bookstore1", function (req, res) {
   });
 });
 
+app.get("/bookstore2", function (req, res) {
+  var bookStore = [
+    {
+      title: "Templating with Pug",
+      author: "Winston Smith",
+      pages: 143,
+      year: 2017
+    },
+    {
+      title: "Node.js will help",
+      author: "Guy Fake",
+      pages: 879,
+      year: 2015
+    },
+    {
+      title: "React.js is awesome",
+      author: "Facebook team",
+      pages: 1666,
+      year: 2014,
+      popup: "Is it popup message?"
+    }
+  ];
+
+  res.render("index", {
+    bookStore: bookStore,
+  });
+});
+
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
--- src/templates/script.js
+++ src/templates/script.js
@@ -1,6 +1,21 @@
 const button = document.getElementById('action-button');
 const container = document.getElementById('container');
+const modal = document.getElementById('modal');
+const modalMessage = modal.querySelector('.modal-body');
+const modalInst = new bootstrap.Modal(modal);
+
+modal.addEventListener('hide.bs.modal', () => {
+  modalMessage.removeChild(modalMessage.firstChild);
+});
 
 button.addEventListener('click', () => {
-  container.querySelector('ul.hidden').classList.remove('hidden');
+  const item = container.querySelector('ul.hidden');
+  const message = item.querySelector('li.popup');
+
+  item.classList.remove('hidden');
+
+  if (message) {
+    modalMessage.appendChild(item.cloneNode(true));
+    modalInst.show();
+  }
 });
